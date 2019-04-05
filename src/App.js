f52669a23278d740ac420ac20efc93643174d7ed
Liam <liamhellis@gmail.com>
2019-04-05 01:39:59 -0400

    set up appsync auth with aws cognito pool

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import Amplify from 'aws-amplify'
+import Amplify, { Auth, Api } from 'aws-amplify'
 import aws_exports from './aws-exports'
 import { withAuthenticator } from 'aws-amplify-react'
 import { Header } from 'semantic-ui-react'
 
 Amplify.configure(aws_exports)
+Auth.configure(Auth)
+console.log('TCL: withAuthenticator', withAuthenticator)
 
 class App extends Component {
   render () {
